825e68d984fbdcf3f2c096bdff70a64e69f00769
Rafael N <65974055+Rxfa@users.noreply.github.com>
2022-07-27 22:53:35 +0100

    completed anecdotes step1

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,9 +1,33 @@
 import { useState } from 'react'
 
+const Button = (props) => (
+  <button onClick={props.handleClick}>next anecdote</button>
+)
+
 const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+
+    var randomValue = Math.floor(Math.random(() => Date.getMilliseconds()) * 6) + 1
+
+  const setSelectedValue = newValue => {
+    setSelected(randomValue)
+    console.log(selected)
+  }
+
   return (
     <div>
-      Hi! [Insert random anecdote]
+      <h2>{anecdotes[selected]}</h2>
+      <Button handleClick={() => setSelectedValue()} />
     </div>
   )
 }
